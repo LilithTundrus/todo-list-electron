df36ccd1db0a7650b59628aad96d4bc24d744ea9
Lilith Tundrus <echofox123@gmail.com>
2018-02-02 22:02:05 -0500

    Adding a modal for text input

--- uiHelper.js
+++ uiHelper.js
@@ -2,9 +2,10 @@
 
 document.getElementById('plusButton').addEventListener('click', function () {
     
+    document.getElementById('inputModal').style.display = 'block';
     let divTopAppend = document.createElement('div')
     divTopAppend.setAttribute('class', 'w3-card');
-    divTopAppend.innerText = 'AAAA'
-    document.getElementById('listContainer').appendChild(divTopAppend)
+    divTopAppend.innerText = 'AAAA';
+    document.getElementById('listContainer').appendChild(divTopAppend);
 
 })
