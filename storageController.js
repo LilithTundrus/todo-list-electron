2566f2e1d1c142406aad0860a828aa42cb72945a
Lilith Tundrus <echofox123@gmail.com>
2018-02-03 17:56:47 -0500

    adding crude localStorage methods for testing

--- storageController.js
+++ storageController.js
@@ -1,28 +1,36 @@
+'use strict';
 // This is where we get and retrieve the local storage for the user's TODO list
 
-// Check browser support
-
-
-// Expose an API here for using non-standard Web calls from index.html
-/* if (typeof (Storage) !== "undefined") {
-    // Store
-    localStorage.setItem("lastname", "Smith");
-    // Retrieve
-    document.getElementById("result").innerHTML = localStorage.getItem("lastname");
-} else {
-    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
-} */
 function retrieveTodoListItems() {
 
 }
 
-function getListCount() {
+function initListCount() {
     // Return the number of items in the todo list
     if (localStorage.listCount) {
+        console.log(localStorage.listCount)
         return localStorage.listCount;
     } else {
         localStorage.setItem('listCount', 0);
+        console.log(localStorage.listCount)
+
     }
 }
 
+function getListCount() {
+    return localStorage.listCount;
+}
+
+function resetListCount() {
+    localStorage.setItem('listCount', 0);
+
+}
+
+module.exports.initListCount = initListCount;
 module.exports.getListCount = getListCount;
+module.exports.resetListCount = resetListCount;
+
+
+module.exports.test = function () {
+    console.log('test worked')
+}
