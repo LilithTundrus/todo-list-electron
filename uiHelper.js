7efcc50e83b21bcb705cdaa97fe54a2302136a06
Lilith Tundrus <echofox123@gmail.com>
2018-02-03 14:57:32 -0500

    got a (veryt broken) remove button working. Next commit wil;l be using localSotrage.

--- uiHelper.js
+++ uiHelper.js
@@ -1,11 +1,31 @@
 'use strict';
 
 document.getElementById('plusButton').addEventListener('click', function () {
-    
+
     document.getElementById('inputModal').style.display = 'block';
-    let divTopAppend = document.createElement('div')
-    divTopAppend.setAttribute('class', 'w3-card');
-    divTopAppend.innerText = 'AAAA';
+
+
+})
+
+document.getElementById('addTodoButton').addEventListener('click', function () {
+    let divTopAppend = document.createElement('div');
+    divTopAppend.setAttribute('class', 'w3-container w3-card');
+    divTopAppend.setAttribute('id', 'test');
+    let todoText = document.getElementById('todoInputText').value;
+    let removeButton = document.createElement('button')
+
+    // Create  the button to then append
+    removeButton.setAttribute('onclick', "document.getElementById('test').style.display='none'")
+    removeButton.setAttribute('class', 'w3-button w3-blue w3-right')
+    removeButton.innerText = 'Remove'
+
+    divTopAppend.innerText = todoText;
     document.getElementById('listContainer').appendChild(divTopAppend);
+    document.getElementById('test').appendChild(removeButton);
+
+    // Re-hide the modal
+    document.getElementById('inputModal').style.display = 'none';
+    //Reset the modal value
+    document.getElementById('todoInputText').value = '';
 
 })
