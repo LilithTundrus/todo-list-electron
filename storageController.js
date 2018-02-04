8df12ee799fec59003a1b3e78accef043348bb19
Lilith Tundrus <echofox123@gmail.com>
2018-02-03 19:11:36 -0500

    More work on getting sotrage methods working. Still kind of a mess but better than before.

--- storageController.js
+++ storageController.js
@@ -2,7 +2,26 @@
 // This is where we get and retrieve the local storage for the user's TODO list
 
 function retrieveTodoListItems() {
+    if (localStorage.listItems) {
+        return JSON.parse(localStorage.listItems);
+    } else {
+        localStorage.setItem('listItems', JSON.stringify(new Array()));
+        return localStorage.listItems;
+    }
+}
 
+function removeTodoListItemByID(id) {
+    // Iterate over the localStorage Array and check for an object match
+    console.log(`Searching for id: ${id}`)
+    let tempListItemsArray = JSON.parse(localStorage.listItems);
+    let newListArray
+    tempListItemsArray.forEach((entry, index) => {
+        if (entry.listItemNumber == id) {
+            newListArray = tempListItemsArray.splice(index, 1)
+        }
+    })
+    // put the new array back into localStorage
+    localStorage.listItems = JSON.stringify(newListArray);
 }
 
 function initListCount() {
@@ -13,7 +32,6 @@ function initListCount() {
     } else {
         localStorage.setItem('listCount', 0);
         console.log(localStorage.listCount)
-
     }
 }
 
@@ -23,12 +41,19 @@ function getListCount() {
 
 function resetListCount() {
     localStorage.setItem('listCount', 0);
+}
 
+function resetListItems() {
+    localStorage.setItem('listItems', JSON.stringify(new Array()));
 }
 
 module.exports.initListCount = initListCount;
 module.exports.getListCount = getListCount;
 module.exports.resetListCount = resetListCount;
+module.exports.resetListItems = resetListItems;
+module.exports.removeTodoListItemByID = removeTodoListItemByID;
+
+module.exports.retrieveTodoListItems = retrieveTodoListItems;
 
 
 module.exports.test = function () {
--- uiHelper.js
+++ uiHelper.js
@@ -1,26 +1,40 @@
 'use strict';
-
+// Display the input modal when the plus button is used
 document.getElementById('plusButton').addEventListener('click', function () {
-
     document.getElementById('inputModal').style.display = 'block';
-
-
 })
+
+let listItems = controller.retrieveTodoListItems();
+
+if (listItems.length > 0) {
+    console.log(listItems)
+}
 
 document.getElementById('addTodoButton').addEventListener('click', function () {
     // TODO: Create some padding for elements
-    // TODO: 
+    // TODO: retrive the old notes
+    // TODO: create a better way of controlling these elements!
     let divTopAppend = document.createElement('div');
     divTopAppend.setAttribute('class', 'w3-container w3-card');
+    // Advanced the number of items by one
     localStorage.listCount++;
     divTopAppend.setAttribute('id', `listItem${controller.getListCount()}`);
     let todoText = document.getElementById('todoInputText').value;
-    let removeButton = document.createElement('button')
+    let removeButton = document.createElement('button');
+    let itemToStore = {
+        // We need to store an ID to reference this by in the storage controller function
+        listItemNumber: controller.getListCount(),
+        text: todoText
+    }
+    listItems.push(itemToStore);
+    // Store the todo value in localStorage
+    localStorage.listItems = JSON.stringify(listItems);
+    console.log(localStorage.listItems)
 
     // Create  the button to then append
-    removeButton.setAttribute('onclick', `document.getElementById('listItem${controller.getListCount()}').style.display='none';localStorage.listCount--;`)
-    removeButton.setAttribute('class', 'w3-button w3-blue w3-right')
-    removeButton.innerText = 'Remove'
+    removeButton.setAttribute('onclick', 'removeItem()');
+    removeButton.setAttribute('class', 'w3-button w3-blue w3-right');
+    removeButton.innerText = 'Remove';
 
     divTopAppend.innerText = todoText;
     document.getElementById('listContainer').appendChild(divTopAppend);
@@ -32,3 +46,13 @@ document.getElementById('addTodoButton').addEventListener('click', function () {
     document.getElementById('todoInputText').value = '';
 
 })
+
+function removeItem() {
+    {
+        document.getElementById(`listItem${controller.getListCount()}`).style.display = 'none';
+        controller.removeTodoListItemByID(controller.getListCount())
+        localStorage.listCount--;
+    }
+}
+
+// Restore the old todo entries
