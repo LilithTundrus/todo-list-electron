1b6c87848fd9e06b650c75df51cb6bb9a0fed4ed
Lilith Tundrus <echofox123@gmail.com>
2018-02-10 23:25:53 -0500

    Adjusted default window dimension, updated the menu to make much more sense for this app.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 const path = require('path')
 const url = require('url')
 
@@ -8,7 +8,7 @@ let win
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({ width: 1024, height: 768 })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -16,6 +16,34 @@ function createWindow() {
     protocol: 'file:',
     slashes: true
   }))
+  const template = [
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+      ]
+    },
+    {
+      role: 'window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'close' }
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Submit an Issue',
+          click() { require('electron').shell.openExternal('https://github.com/LilithTundrus/todo-list-electron/issues') }
+        }
+      ]
+    }
+  ]
+  const menu = Menu.buildFromTemplate(template);
+  // Menu.setApplicationMenu(menu);
+  win.setMenu(menu);
+
 
   // Open the DevTools.
   //win.webContents.openDevTools()
